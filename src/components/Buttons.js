52ec485fc18ff6606873a18281cb217bdbf0f9f8
iamsaad2 <saadbadat.1@gmail.com>
2020-06-27 14:13:13 -0400

    buttons

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -8,30 +8,23 @@ const Buttons = (props) => {
   let foo = url.searchParams.get('url') + window.location.hash;
 
   let link = foo.slice(6);
+  console.log(`pktc://subscribe${link.slice(1)}`);
 
   return (
     <div className='buttons'>
       <div className='button-container'>
-        <form action={`https://helloaudio.fm/subscribe?url=podcast:${link}`}>
+        <form action={`podcast:${link}`}>
           <button type='submit' className='button'>
             APPLE PODCASTS
           </button>
         </form>
-        <form
-          action={`https://helloaudio.fm/subscribe?url=overcast://x-callback-url/add?url=https:${link}`}
-        >
+        <form action={`overcast://x-callback-url/add?url=https:${link}`}>
           <button className='button'>OVERCAST</button>
         </form>
-        <form
-          action={`https://helloaudio.fm/subscribe?url=podcastaddict:${link}`}
-        >
+        <form action={`podcastaddict:${link}`}>
           <button className='button'>PODCAST ADDICT</button>
         </form>
-        <form
-          action={`https://helloaudio.fm/subscribe?url=pktc://subscribe${link.slice(
-            1
-          )}`}
-        >
+        <form action={`pktc://subscribe${link.slice(1)}`}>
           <button className='button'>POCKET CASTS</button>
         </form>
         <CopyToClipboard text={foo}>
